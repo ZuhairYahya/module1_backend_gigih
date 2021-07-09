498d9ebe050a5c4269bba1c25a8f55617292da14
zuhairyahya <zuhairyahya57@gmail.com>
2021-07-09 10:47:55 +0700

    upload session 2

---
+++ handons/mammal.rb
@@ -0,0 +1,16 @@
+#inheritance
+class Mammal
+    def breathe
+        puts "inhale and exhale"
+    end
+end
+
+class Cat < Mammal
+    def speak
+        puts "Meow"
+    end
+end
+
+neko = Cat.new
+neko.breathe
+neko.speak
